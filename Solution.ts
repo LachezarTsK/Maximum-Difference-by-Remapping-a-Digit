635f2c82a739d0c99112922e246f9ad18cd9ad31
Lachezar Kutsarov <lkutsarov@gmail.com>
2025-06-14 19:55:00 +0200

    Create Solution.ts

---
+++ Solution.ts
@@ -0,0 +1,45 @@
+
+function minMaxDifference(input: number): number {
+    const numberOfDigits = findNumberOfDigits(input);
+    const minValue = createExtremeValue(input, Util.DIGIT_TO_INSERT_FOR_MIN_VALUE, numberOfDigits);
+    const maxValue = createExtremeValue(input, Util.DIGIT_TO_INSERT_FOR_MAX_VALUE, numberOfDigits);
+
+    return maxValue - minValue;
+};
+
+class Util {
+    static NOT_FOUND = -1;
+    static DIGIT_TO_INSERT_FOR_MIN_VALUE = 0;
+    static DIGIT_TO_INSERT_FOR_MAX_VALUE = 9;
+}
+
+function findNumberOfDigits(input: number): number {
+    let numberOfDigits = 0;
+    while (input > 0) {
+        input = Math.floor(input / 10);
+        ++numberOfDigits;
+    }
+    return (numberOfDigits !== 0) ? numberOfDigits : 1;
+}
+
+function createExtremeValue(input: number, digitToInsertForExtremeValue: number, dumberOfDigits: number): number {
+    let extremeValue = 0;
+    let digitToReplace = Util.NOT_FOUND;
+    let extractorForLeftmostValue = Math.pow(10, dumberOfDigits - 1);
+
+    while (extractorForLeftmostValue > 0) {
+        let leftmostDigit = Math.floor(input / extractorForLeftmostValue);
+
+        if (digitToReplace === Util.NOT_FOUND && leftmostDigit !== digitToInsertForExtremeValue) {
+            digitToReplace = leftmostDigit;
+        }
+        if (leftmostDigit === digitToReplace) {
+            leftmostDigit = digitToInsertForExtremeValue;
+        }
+
+        extremeValue = 10 * extremeValue + leftmostDigit;
+        input %= extractorForLeftmostValue;
+        extractorForLeftmostValue = Math.floor(extractorForLeftmostValue / 10);
+    }
+    return extremeValue;
+}
